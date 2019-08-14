ad0beb38949a068ea2c2ceab2703c98abdd4a308
Colin Bell <colin@dogoodercomics.co.uk>
2019-08-14 12:53:23 +0100

    app can now subscribe/unsubscribe from subs at will

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,24 @@
   <div id="app">
     <h1>readditor</h1>
     <input v-model="subreddit" type="text" name="subreddit">
-    <button v-on:click="getPosts"type="button" name="button"></button>
+    <button v-on:click="getPosts"type="button" name="button">Subscribe</button>
     <div id="feeds">
       <div v-for="(posts, sub) in posts" class="feed">
-        <h1>r/{{sub}} <button v-on:click="update(sub)">Refresh</button></h1>
-        <ul>
-          <li v-for="post in posts"><strong>{{post.title}}</strong><button v-on:click="selectPost(post)">Read This</button></li>
-        </ul>
+        <h1>r/{{sub}}</h1>
+        <span><button v-on:click="update(sub)">Refresh</button><button v-on:click="unsubscribe(sub)">Unsubscribe</button></span>
+        <div class="posts">
+          <ul>
+            <li v-for="post in posts"><strong>{{post.data.title}}</strong>  <button v-on:click="selectPost(post)">View</button></li>
+          </ul>
+        </div>
       </div>
     </div>
-    <modal name="post-container">
-      {{selectedPost.text}}
-      <button @click="hide"type="button" name="button">Close</button>
+    <modal v-if="selectedPost" name="post-container">
+      {{selectedPost.data.selftext}}
+      <button @click="hide('post-container')"type="button" name="button">Close</button>
+    </modal>
+    <modal name="loading">
+      Loading...
     </modal>
   </div>
 </template>
@@ -24,17 +30,19 @@ export default {
   name: 'app',
   data(){
     return {
+      loading: false,
       subreddit: "",
+      subreddits: [],
       posts: {},
       selectedPost: null
     }
   },
   methods: {
-    show () {
-      this.$modal.show('post-container');
+    show (modalName) {
+      this.$modal.show(modalName);
     },
-    hide () {
-      this.$modal.hide('post-container');
+    hide (modalName) {
+      this.$modal.hide(modalName);
     },
     update: function(sub){
       this.subreddit = sub
@@ -42,23 +50,27 @@ export default {
     },
     selectPost: function(post){
       this.selectedPost = post
-      this.show()
+      this.show('post-container')
+    },
+    unsubscribe: function(sub){
+      this.$delete(this.posts, sub)
     },
     getPosts: function(){
+      this.loading = true
       fetch(`https://www.reddit.com/r/${this.subreddit}.json`)
       .then(res => res.json())
       .then(posts => {
-        const customPosts = posts.data.children.map(post => {
-          return {
-            title: post.data.title,
-            text: post.data.selftext,
-            user: post.data.author,
-            url: post.data.url
-          }
-        })
-        this.posts[this.subreddit] = customPosts
+        this.posts[this.subreddit] = posts.data.children
+        this.subreddits.push(this.subreddit)
         this.subreddit = ""
+        this.loading = false
+        //TODO: Error handling
       })
+    }
+  },
+  computed: {
+    isLoading: function(){
+      this.loading ? this.show('loading') : this.hide('loading')
     }
   }
 }
@@ -69,4 +81,15 @@ export default {
   display: flex;
   flex-direction: wrap;
 }
+
+.feed {
+  padding: 20px;
+}
+
+.posts {
+  overflow-y: scroll;
+  height: 300px;
+  width: 300px;
+}
+
 </style>
